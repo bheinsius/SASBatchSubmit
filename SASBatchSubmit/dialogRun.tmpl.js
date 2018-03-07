dbe2f82b61f74cc9e245d871621fd0e59807f461
bheinsius <bheinsius@eom.nl>
2018-03-07 23:04:32 +0100

    escape sas code before sending it to sas

--- SASBatchSubmit/dialogRun.tmpl.js
+++ SASBatchSubmit/dialogRun.tmpl.js
@@ -73,11 +73,11 @@ function RunDialogController($scope,$mdDialog,$http,$log,$cookies,sascode) {
   getBatchServers();
 
   // to split SAS code into chunks where it gets larger than 32000 characters
-  function chunkSubstr(str, size) {
+  function chunkSubstr(str,size) {
     const numChunks = Math.ceil(str.length / size)
     const chunks = new Array(numChunks)
     for (let i = 0, o = 0; i < numChunks; ++i, o += size) {
-      chunks[i] = str.substr(o, size)
+      chunks[i] = escape(str.substr(o,size));
     }
     return chunks
   }
