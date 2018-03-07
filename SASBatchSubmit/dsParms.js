69b3c72670f7104fdf0af0f26987b4081cd91904
bheinsius <bheinsius@eom.nl>
2018-03-07 17:57:07 +0100

    corrected type

--- SASBatchSubmit/dsParms.js
+++ SASBatchSubmit/dsParms.js
@@ -9,7 +9,7 @@ function urlSTP(stp) {
 }
 
 // if you use a restricted shell for shell escape, you cannot specify '/' in command names.
-// in that case set useRestrictedShellCommands = true and the shortcut commands will be used.
+// in that case set useShortcutCommands = true and the shortcut commands will be used.
 // to make this work make shortcut scripts that call the cmdline and that exist in the path, e.g., /usr/local/bin.
 var useShortcutCommands = false;
 var cmdlineShortcuts = [{cmdline:"/apps/sas/SASConfig/Lev5/SASApp/BatchServer/sasbatch.sh", shortcut:"Lev5_SASApp_BatchServer.sh"}
